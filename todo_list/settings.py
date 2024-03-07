dbd445466cfeb10f5529b8b04ceca4cc4160a99e
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-07 17:42:47 +0200

    templates for tasks and tags add bootstrap to settings

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "task",
 ]
 
@@ -130,3 +132,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
